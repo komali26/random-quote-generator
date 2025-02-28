f293103f9f966cf172a65ffc5d48d6a291c90042
komali26 <komali.balusu26@gmail.com>
2025-02-28 20:28:57 +0530

    Added quote generator script

---
+++ quote_generator.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Believe in yourself!",
+    "Keep pushing forward.",
+    "Stay positive, work hard, make it happen.",
+    "You are stronger than you think.",
+    "Dream big and dare to fail."
+]
+
+print(random.choice(quotes))
